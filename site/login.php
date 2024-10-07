26d806939916c5252a45c372a9c89284c5abbea1
TheRefraction <the.r3fraction@gmail.com>
2024-10-07 23:12:36 +0200

    Project Structure
    And also database (SQL Format)

---
+++ site/login.php
@@ -0,0 +1,22 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Martine Travels - Login</title>
+    </head>
+	
+    <body>
+        <h1> Log into your Martine Travels account</h1>
+        <form method="post" action="url">
+            <label>
+                Login : <input type="text" name="login" />
+            </label>
+            <br />
+            <label>
+                Password : <input type="password" name="password" required="required" />
+            </label>
+            <br />
+            <input type="submit" value="Submit" />
+        </form>
+    </body>
+
+</html>
---
+++ site/register.php
@@ -0,0 +1,11 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Martine Travels - Register</title>
+    </head>
+	
+    <body>
+
+    </body>
+
+</html>
