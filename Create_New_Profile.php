b59121a2d6a17b6a483b82b00dfbab15c2682436
The Refraction <106912968+TheRefraction@users.noreply.github.com>
2024-10-07 10:23:57 +0200

    Update Create_New_Profile.php

--- Create_New_Profile.php
+++ Create_New_Profile.php
@@ -8,35 +8,33 @@
         <label>
             FirstName : <input type="text" name="first_name" />
         </label>
-        <br />
+        <br>
         <label>
             Name : <input type="text" name="name" />
         </label>
-        <br />
-
+        <br>
         <label>
             Email : <input type="email" placeholder="email@domain.com" name="email" required="required" />
         </label>
-        </br>
+        <br>
         <label>
             Phone number : <input type="number" placeholder="0545654751" name="phone_number" required="required" />
         </label>
-        </br>
+        <br>
         <label>
             Password : <input type="password" name="password" required="required" />
         </label>
-        </br>
+        <br>
         <label>
             Password's Confirmation : <input type="password" name="password_confirmation" required="required" />
         </label>
-        <br />
-
-        <br />
+        <br>
+        <br>
         <label>
             J'accepte de recevoir des infos et des pubs
             <input type="checkbox" name="pub" />
         </label>
-        <br />
+        <br>
         <input type="submit" value="Submit" />
         </form>
     </body>
