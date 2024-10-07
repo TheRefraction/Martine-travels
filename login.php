8c19fba81673be7f8d48d8bf9bd2b51a9c0ed935
lorna <lorna.jourdan@utbm.fr>
2024-10-07 09:04:51 +0200

    :)

--- login.php
+++ login.php
@@ -1,1 +1,22 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Martine's Travels</title>
+    </head>
+    <body>
+        <h1>connexion to Martine's Travels</h1>
+        <form method="post" action="url">
+            <label>
+                login : <input type="text" name="login" />
+            </label>
+            <br />
+            <label>
+                Password : <input type="password" name="password" required="required" />
+            </label>
+            <br />
+            <input type="submit" value="Submit" />
+        </form>
 
+    </body>
+
+</html>
