cd18a7c21270fa3b7cb415bc70e18c58ffbd65ca
lorna <lorna.jourdan@utbm.fr>
2024-10-08 08:39:23 +0200

    ;)

--- site/add_client.php
+++ site/add_client.php
@@ -1,15 +1,23 @@
 <?php
 
-$name = $_POST['name'];
-$first_name = $_POST['first_name'];
-$phone_number = $_POST['phone_number'];
-$email = $_POST['email'];
+$Last_name = $_POST['Last_name'];
+$First_name = $_POST['First_name'];
+$Phone = $_POST['Phone'];
+$Email = $_POST['Email'];
 $password = $_POST['password'];
+$Birth_date = $_POST['Birth_date'];
+$password_confirmation = $_POST['password_confirmation'];
 
-$bdd = new PDO("mysql:host=localhost;dbname=account;charset=utf8", "root", "");
-$req = $bdd->prepare("INSERT INTO client(name, first_name, phone_number, email, password) VALUES (?,?,?,?,?);");
+if ($password !== $password_confirmation) {
+    echo "Password are not the same. Try again.";
+} else {
+    $Password_confirmation = password_hash($password, PASSWORD_BCRYPT);
 
-$req->execute([$name, $first_name,$phone_number,$email,$password]);
+    $bdd = new PDO("mysql:host=localhost;dbname=Martine_travels;charset=utf8", "root", "");
+    $req = $bdd->prepare("INSERT INTO User(First_name, Last_name, Email, Phone, Birth_date) VALUES (?,?,?,?,?);");
 
+    $req->execute([$First_name, $Last_name, $Email, $Phone, $Birth_date]);
+    echo "Inscription successful, you can go to the login page. ";
+}
 die();
 ?>
--- site/info_client.php
+++ site/info_client.php
@@ -6,19 +6,23 @@
     <h1>Creation of a new account</h1>
     <form method="post" action="add_client.php">
         <label>
-            FirstName : <input type="text" name="first_name" />
+            First Name : <input type="text" name="First_name" />
         </label>
         <br>
         <label>
-            Name : <input type="text" name="name" />
+            Last Name : <input type="text" name="Last_name" />
         </label>
         <br>
         <label>
-            Email : <input type="email" placeholder="email@domain.com" name="email" required="required" />
+            Email : <input type="email" placeholder="email@domain.com" name="Email" required="required" />
         </label>
         <br>
         <label>
-            Phone number : <input type="number" placeholder="0545654751" name="phone_number" required="required" />
+            Phone number : <input type="number" placeholder="0545654751" name="Phone" required="required" />
+        </label>
+        <br>
+        <label>
+            Birthdate : <input type="date" name="Birth_date" required="required" />
         </label>
         <br>
         <label>
@@ -29,11 +33,6 @@
             Password's Confirmation : <input type="password" name="password_confirmation" required="required" />
         </label>
         <br>
-        <br>
-        <label>
-            J'accepte de recevoir des infos et des pubs
-            <input type="checkbox" name="pub" />
-        </label>
         <br>
         <input type="submit" value="Submit" />
         </form>
