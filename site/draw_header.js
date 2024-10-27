09a5d5fa92a3b90b10dd88820341a5310b7cf557
lorna <lorna.jourdan@utbm.fr>
2024-10-27 18:17:03 +0100

    j'ai résolu le probleme de taille de boite

--- site/draw_header.js
+++ site/draw_header.js
@@ -3,6 +3,6 @@ document.write(`\
         <img src="img/Logo_Martine.png" alt="Martine travels logo" class="logo">\
         <a href=index.html>Home</a>\
         <a href="signin.html">Sign in</a>\
-        <img src="https://placehold.co/20x20" alt="button parameter" class="logo2">\
+        
     </header>\
 `);
