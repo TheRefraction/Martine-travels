738b3ba1b4c28a90c91cf31a4244c5aed5e14025
Maxime <maxime.giraux@xuarig.fr>
2024-10-27 20:22:05 +0100

    modif home
    Signed-off-by: Maxime <maxime.giraux@xuarig.fr>

--- site/home.php
+++ site/home.php
@@ -1,3 +1,6 @@
+
+
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -13,9 +16,14 @@
 <section class ="login_section">
     <h1> Welcome to your home page</h1><br>
 
+    <form method="post" action="add_reservation.php">
     <label>
         <input type="submit" name='home_ajout' value="Add a reservation" /><br>
     </label><br>
+
+    </form>
+
+    <form method="post" action="modify_reservation.php">
     <label>
         <input type="submit" name='home_modify' value="Modify a reservation" /><br><br>
     </label><br>
@@ -25,41 +33,51 @@
     <?php
 
     include("connection.php");
+    $user=9; //A modifier pour avoir l'user
+
     $bdd = get_dbhandle();
 
-
-
     //Acquisition of data from previous packages
-    //$req = $bdd->prepare("SELECT * FROM `previouspackage` Pr INNER JOIN 'package' Pa ON Pr.Package_ID = Pa.ID;");
-    //$req->execute();
+    $req = $bdd->prepare("SELECT * FROM Package_User Pr INNER JOIN Package Pa ON Pr.Package_ID = Pa.ID INNER JOIN Destination De ON De.ID = Pa.Destination_ID WHERE Status = 3 AND Pr.User_ID=$user;");
+    $req->execute();
+
     ?>
 
-    <table>
-        <tr>
-            <th>Destination</th>
-            <th>Duration</th>
-            <th>Price</th>
-        </tr>
-
+    <table class="table_design">
+        <thead>
+            <tr>
+                <th>Destination</th>
+                <th>Duration</th>
+                <th>Price</th>
+            </tr>
+        </thead>
+        <tbody>
         <?php
-        /*
+
         while($data = $req->fetch())
         {
             ?>
             <tr>
-                <td><?php echo $data["Destination_ID"]; ?></td>
-                <td><?php echo $data["Duration"]; ?></td>
-                <td><?php echo $data["Price"]; ?></td>
+                <td><?php echo $data["Name"]; ?></td>
+                <td><?php echo $data["Duration"]; ?> days</td>
+                <td><?php echo $data["Price"]; ?>€</td>
             </tr>
             <?php
 
         }
-        */
+
         ?>
 
+        </tbody>
     </table>
 
 </section>
+
+<?php
+if(isset($_POST['home_ajout'])){
+    header("location:add_reservation.php");
+}
+?>
 
 </body>
 
---
+++ site/modify_reservation.php
@@ -0,0 +1,20 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Martine Travels - Home</title>
+    <link href ="css/style.css" rel="stylesheet">
+</head>
+
+
+<body>
+
+<script src="draw_header.js"></script>
+
+
+<?php
+?>
+
+
+</body>
+
+</html>
