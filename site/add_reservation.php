04a923545b49db2530bd9638114c94ef3c63cfd5
Maxime <maxime.giraux@xuarig.fr>
2024-11-04 20:25:08 +0100

    transportation debut
    Signed-off-by: Maxime <maxime.giraux@xuarig.fr>

--- site/add_reservation.php
+++ site/add_reservation.php
@@ -40,7 +40,7 @@ if (!isset($_SESSION['email'])) {
 
         <!-- Custom -->
         <div id="labelCustom" class="label" hidden>
-            <label><br>
+            <div><br>
                 <label for="pays">Choose where you want to go :</label><br>
                 <select id="pays" onchange="accomodation()">
                     <option value="NULL">----- Choisissez une option ----</option>
@@ -53,28 +53,63 @@ if (!isset($_SESSION['email'])) {
                     $req->execute();
 
                     while ($data = $req->fetch()) {
-                        echo '<option value="' .$data["ID"]. '">' . htmlspecialchars($data["Name"]) . '</option>';
+                        echo '<option value="' .$data["ID"]. '">' . $data["Name"] . '</option>';
                     }
                     ?>
 
-                </select>
-                <br><br>
+            </select>
+            <br><br>
 
-
-                <label id="label_accomodation" for="accomodation" hidden>Choose in which of the following accomodation (in the country) you want to go :</label><br>
-                <select id="accomodation" hidden>
+            <div id="div_accomodation" hidden>
+                <label id="label_accomodation" for="accomodation">Choose in which of the following accomodation (in the country) you want to go :</label><br>
+                <select id="accomodation" onchange="transportation()">
                     <option value="NULL">-- Choisissez une option --</option>
-
-
                 </select><br>
 
+                <div id="div_type_transportation" hidden><br>
+                    <label id="label_type_transportation" for="type_transportation">Choose the type of transportation :</label><br>
+                    <select id="type_transportation">
+                        <option value="NULL">-- Choisissez une option --</option>
 
-            </label>
+                        <?php
+
+                        $bdd = get_dbhandle();
+                        $req = $bdd->prepare("SELECT Name From Transport;");
+                        $req->execute();
+
+                        while ($data = $req->fetch()) {
+                            echo '<option value="' .$data["ID"]. '">' . $data["Name"] . '</option>';
+                        }
+                        ?>
+
+                    </select><br><br>
+
+                    <form method="post" onchange="">
+                        <label>
+                            <input type="submit" name='transportation_ajout' value="Add a transportation" /><br>
+                        </label><br>
+                    </form><br>
+
+                    <form method="post" onchange="">
+                        <label>
+                            <input type="submit" name='transportation_Remove' value="Remove a transportation" /><br>
+                        </label><br>
+                    </form>
+
+                </div>
+
+
+
+                </form>
+
+            </div>
+
+            </div><br>
+
         </div>
 
-
             <!-- Premade-->
-        <div id="labelPremade" class="label" hidden>
+        <div id="labelPremade" hidden>
             <br>
             <label for="package_choice">Choose the package</label><br>
             <select id="package_choice">
@@ -82,13 +117,11 @@ if (!isset($_SESSION['email'])) {
 
 
                 <?php
-
-                //$bdd = get_dbhandle();
                 $req = $bdd->prepare("SELECT De.Name, De.ID From Package Pa INNER JOIN Destination De ON De.ID = Pa.Destination_ID;");
                 $req->execute();
 
                 while ($data = $req->fetch()) {
-                    echo '<option value="' . htmlspecialchars($data["ID"]) . '">' .  htmlspecialchars($data["Name"]) . '</option>';
+                    echo '<option value="' . $data["ID"] . '">' .  $data["Name"] . '</option>';
                 }
 
                 ?>
@@ -113,20 +146,15 @@ if (!isset($_SESSION['email'])) {
                     labelPremade.hidden = false;
                 }
             }
-        </script>
-
-        <script>
 
             function accomodation(){
 
                 const paysSelect = document.getElementById("pays");
                 const accommodationSelect = document.getElementById("accomodation");
-                const accommodationLabel = document.getElementById("label_accomodation");
-                accommodationSelect.hidden = true;
-                accommodationLabel.hidden =true;
+                const accommodationDiv = document.getElementById("div_accomodation");
+                accommodationDiv.hidden =true;
                 if (paysSelect.value!=="NULL") {
-                    accommodationSelect.hidden = false;
-                    accommodationLabel.hidden = false;
+                    accommodationDiv.hidden = false;
 
 
 
@@ -142,6 +170,16 @@ if (!isset($_SESSION['email'])) {
                             });
                         })
 
+                }
+            }
+
+            function transportation(){
+                const accomodation = document.getElementById("accomodation");
+                const TransportDiv = document.getElementById("div_type_transportation");
+
+                TransportDiv.hidden = true;
+                if(accomodation.value !== "NULL"){
+                    TransportDiv.hidden = false;
                 }
             }
 
