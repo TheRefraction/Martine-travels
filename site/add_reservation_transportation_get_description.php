9ba065db06662be92d23f286d5b7bb71a7a38590
Maxime <maxime.giraux@xuarig.fr>
2024-11-12 19:53:15 +0100

    small changes
    Signed-off-by: Maxime <maxime.giraux@xuarig.fr>

--- site/add_reservation_transportation_get_description.php
+++ site/add_reservation_transportation_get_description.php
@@ -19,32 +19,19 @@ if (isset($_GET["type"]) and isset($_GET["departure"]) and isset($_GET["arrival"
     Address_Departure_Street.Name AS Departure_Street,
     Provider.Name AS Provider,
     Tp.*
-    FROM 
-        Transportation Tp
-    INNER JOIN 
-        Transportation_Type TpT ON Tp.Type_ID = TpT.ID
-    INNER JOIN 
-        Address Ad_Arrival ON Tp.Address_Arrival_ID = Ad_Arrival.ID
-    INNER JOIN 
-        Address Address_Departure ON Tp.Address_Depature_ID = Address_Departure.ID
-    INNER JOIN 
-        Address_Country AS Address_Arrival_Country ON Address_Arrival_Country.ID = Ad_Arrival.Country_ID
-    INNER JOIN 
-        Address_County AS Address_Arrival_County ON Address_Arrival_County.ID = Ad_Arrival.County_ID
-    INNER JOIN 
-        Address_Town AS Address_Arrival_Town ON Address_Arrival_Town.ID = Ad_Arrival.Town_ID
-    INNER JOIN 
-        Address_Street AS Address_Arrival_Street ON Address_Arrival_Street.ID = Ad_Arrival.Street_ID
-    INNER JOIN 
-        Address_Country AS Address_Departure_Country ON Address_Departure_Country.ID = Address_Departure.Country_ID
-    INNER JOIN 
-        Address_County AS Address_Departure_County ON Address_Departure_County.ID = Address_Departure.County_ID
-    INNER JOIN 
-        Address_Town AS Address_Departure_Town ON Address_Departure_Town.ID = Address_Departure.Town_ID
-    INNER JOIN 
-        Address_Street AS Address_Departure_Street ON Address_Departure_Street.ID = Address_Departure.Street_ID
-    INNER JOIN 
-        Transportation_Provider AS Provider ON Tp.Provider_ID = Provider.ID
+    FROM Transportation Tp
+    INNER JOIN Transportation_Type TpT ON Tp.Type_ID = TpT.ID
+    INNER JOIN Address Ad_Arrival ON Tp.Address_Arrival_ID = Ad_Arrival.ID
+    INNER JOIN Address Address_Departure ON Tp.Address_Depature_ID = Address_Departure.ID
+    INNER JOIN Address_Country AS Address_Arrival_Country ON Address_Arrival_Country.ID = Ad_Arrival.Country_ID
+    INNER JOIN Address_County AS Address_Arrival_County ON Address_Arrival_County.ID = Ad_Arrival.County_ID
+    INNER JOIN Address_Town AS Address_Arrival_Town ON Address_Arrival_Town.ID = Ad_Arrival.Town_ID
+    INNER JOIN Address_Street AS Address_Arrival_Street ON Address_Arrival_Street.ID = Ad_Arrival.Street_ID
+    INNER JOIN Address_Country AS Address_Departure_Country ON Address_Departure_Country.ID = Address_Departure.Country_ID
+    INNER JOIN Address_County AS Address_Departure_County ON Address_Departure_County.ID = Address_Departure.County_ID
+    INNER JOIN Address_Town AS Address_Departure_Town ON Address_Departure_Town.ID = Address_Departure.Town_ID
+    INNER JOIN Address_Street AS Address_Departure_Street ON Address_Departure_Street.ID = Address_Departure.Street_ID
+    INNER JOIN Transportation_Provider AS Provider ON Tp.Provider_ID = Provider.ID
     WHERE 
        Tp.Address_Depature_ID = :departure AND Tp.Address_Arrival_ID = :arrival AND Tp.Type_ID=:type AND DATE(Tp.Date_Departure) = :date;
             ");
