35fee6bcd750efb86d1a0bb5f7bc1c165b0e3f05
lorna <lorna.jourdan@utbm.fr>
2024-11-17 02:32:51 +0100

    parce que c'est un probleme

--- site/process_signin.php
+++ site/process_signin.php
@@ -2,7 +2,7 @@
     session_start();
 
     include_once("connection.php");
-    $_SESSION['email']=$_POST['email'];
+
 
 
     $email = $_POST['email'];
@@ -13,16 +13,17 @@
     $req = $bdd->prepare("SELECT * FROM User WHERE Email = ?");
     $req->execute([$email]);
     $data = $req->fetch();
-    $_SESSION['ID']=$data['ID'];
+
     if(empty($data)) {
-        echo "Authentication failed!\nPlease try again <a href='signin.php'>Here</a>.";
+        echo "Authentication failed!\nPlease      try again <a href='signin.php'>Here</a>.";
         exit();
     }
 
     if (password_verify($password, $data['Password'])) {
         $req = $bdd->prepare("UPDATE User SET Last_login = NOW() WHERE Email = ?");
         $req->execute([$email]);
-
+        $_SESSION['email']=$_POST['email'];
+        $_SESSION['ID']=$data['ID'];
         if($data['Is_Admin']) {
             header("Location: admin/admin.php");
         } else {
@@ -30,6 +31,6 @@
         }
         exit();
     } else {
-        echo "Authentication failed!\nPlease try again <a href='signin.php'>Here</a>.";
+        echo "Authentication fuyailed!\nPlease try again <a href='signin.php'>Here</a>.";
     }
 ?>
